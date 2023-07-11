7d665ae303ab9972b8e262ae90062f6de6642601
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-07-12 00:18:25 +0300

    Added hasNextInt to Task Counter Strike

--- src/main/java/ua/hillel/dolhykh/homeworks/homework4/Main.java
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework4/Main.java
@@ -15,27 +15,44 @@ public class Main {
         int[] fragsPlayersFirstTeam = new int[5];
         for (int i = 0; i < 5; i++) {
             System.out.print("Введіть кількість фрагів для гравця " + (i + 1) + ": ");
-            fragsPlayersFirstTeam[i] = scanner.nextInt();
+            int userValueInt = 1;
+            if (scanner.hasNextInt()) {
+                userValueInt = scanner.nextInt();
+            } else {
+                System.out.println("Wrong data!");
+                System.exit(0);
+            }
+            fragsPlayersFirstTeam[i] = userValueInt;
+
         }
-        scanner.nextLine();
 
         // Введення імені другої команди
         System.out.print("Введіть ім'я другої команди: ");
+        scanner.nextLine(); // Consume the newline character
         String teamPittsburghPinguins = scanner.nextLine();
+
         // Введення кількості фрагів для гравців другої команди
         int[] fragsPlayersSecondTeam = new int[5];
         for (int i = 0; i < 5; i++) {
             System.out.print("Введіть кількість фрагів для гравця " + (i + 1) + ": ");
-            fragsPlayersSecondTeam[i] = scanner.nextInt();
+            int userValueInt = 1;
+            if (scanner.hasNextInt()) {
+                userValueInt = scanner.nextInt();
+            } else {
+                System.out.println("Wrong data!");
+                System.exit(0);
+            }
+            fragsPlayersSecondTeam[i] = userValueInt;
+
         }
 
         // Обчислення середнього арифметичного балів кожної команди
-        double averageFirstTeam = calculateAverage(fragsPlayersFirstTeam);
-        double averageSecondTeam = calculateAverage(fragsPlayersSecondTeam);
+        int averageFirstTeam = calculateAverage(fragsPlayersFirstTeam);
+        int averageSecondTeam = calculateAverage(fragsPlayersSecondTeam);
 
         // Визначення переможця
         String winningTeam;
-        double result;
+        int result;
         if (averageFirstTeam > averageSecondTeam) {
             result = averageFirstTeam - averageSecondTeam;
             winningTeam = teamBlackHawks;
@@ -46,18 +63,20 @@ public class Main {
             result = 0;
             winningTeam = "Нічия";
         }
-        // Виведення результатів
-        System.out.println("Перемогла команда " + winningTeam + " набрала " + result + " очків.");
 
-        scanner.close();//Закриваємо сканер
+        // Виведення результатів
+        System.out.println("Перемогла команда " + winningTeam + ", набравши " + result + " очків.");
+
+        scanner.close(); // Закриваємо сканер
     }
 
     // Метод для обчислення середнього арифметичного балів
-    public static double calculateAverage(int[] scores) {
+    public static int calculateAverage(int[] scores) {
         int sum = 0;
         for (int score : scores) {
             sum += score;
         }
-        return (double) sum / scores.length;
+        return sum / scores.length;
     }
 }
+
