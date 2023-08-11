c654f9c6164b8a828b1cf8f806681626f36111ea
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-08-11 22:32:55 +0300

    aligned the code.

---
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant2/ClassicMusic.java
@@ -0,0 +1,8 @@
+package ua.hillel.dolhykh.homeworks.homework12.variant2;
+
+public class ClassicMusic extends MusicStyles {
+    @Override
+    public void playMusic() {
+        System.out.println("Виконується класична музика");
+    }
+}
---
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant2/PopMusic.java
@@ -0,0 +1,8 @@
+package ua.hillel.dolhykh.homeworks.homework12.variant2;
+
+public class PopMusic extends MusicStyles {
+    @Override
+    public void playMusic() {
+        System.out.println("Виконується поп-музика");
+    }
+}
