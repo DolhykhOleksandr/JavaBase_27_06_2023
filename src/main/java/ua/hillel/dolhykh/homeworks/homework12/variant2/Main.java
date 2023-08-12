328615f2d4c4e9382c23e1fde0954c47ce008e85
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-08-12 18:08:52 +0300

    changed the type name

--- src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant2/Main.java
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant2/Main.java
@@ -2,9 +2,9 @@ package ua.hillel.dolhykh.homeworks.homework12.variant2;
 
 public class Main {
     public static void main(String[] args) {
-        MusicStyles popBand = new PopMusic();
-        MusicStyles rockBand = new RockMusic();
-        MusicStyles classicBand = new ClassicMusic();
+        PopMusic popBand = new PopMusic();
+        RockMusic rockBand = new RockMusic();
+        ClassicMusic classicBand = new ClassicMusic();
 
         for (MusicStyles musicStyle : new MusicStyles[]{popBand, rockBand, classicBand}) {
             musicStyle.playMusic();
