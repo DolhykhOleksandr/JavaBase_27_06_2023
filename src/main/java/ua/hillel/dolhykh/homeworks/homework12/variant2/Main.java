437d7c48c23202b6b6fab76c442a3790e562d5d8
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-08-12 19:02:17 +0300

    changed variable name

--- src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant2/Main.java
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant2/Main.java
@@ -2,14 +2,10 @@ package ua.hillel.dolhykh.homeworks.homework12.variant2;
 
 public class Main {
     public static void main(String[] args) {
-        PopMusic popBand = new PopMusic();
-        RockMusic rockBand = new RockMusic();
-        ClassicMusic classicBand = new ClassicMusic();
+        MusicStyles[] musicStyles = {new PopMusic(), new RockMusic(), new ClassicMusic()};
 
-        MusicStyles[] musicStyles = {popBand, rockBand, classicBand};
-
-        for (MusicStyles musicStyle : musicStyles) {
-            musicStyle.playMusic();
+        for (MusicStyles group : musicStyles) {
+            group.playMusic();
         }
     }
 }
