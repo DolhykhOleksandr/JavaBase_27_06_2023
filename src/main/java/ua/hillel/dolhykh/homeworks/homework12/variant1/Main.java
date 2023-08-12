e95ea9b33bab11073442764770e6a20785cd7a73
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-08-12 19:16:51 +0300

    corrected minor nuances Main method

--- src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant1/Main.java
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant1/Main.java
@@ -1,15 +1,15 @@
 package ua.hillel.dolhykh.homeworks.homework12.variant1;
 
 public class Main {
-    public static void main(String[] args) {
-        MusicStyles[] musicBands = {
-                new PopMusic(),
-                new RockMusic(),
-                new ClassicMusic()
-        };
+        public static void main(String[] args) {
+            PopMusic popBand = new PopMusic();
+            RockMusic rockBand = new RockMusic();
+            ClassicMusic classicBand = new ClassicMusic();
 
-        for (MusicStyles band : musicBands) {
-            band.playMusic();
+            MusicStyles[] musicStyles = {popBand, rockBand, classicBand};
+
+            for (MusicStyles musicStyle : musicStyles) {
+                musicStyle.playMusic();
+            }
         }
     }
-}
--- src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant1/MusicStyles.java
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework12/variant1/MusicStyles.java
@@ -2,7 +2,7 @@ package ua.hillel.dolhykh.homeworks.homework12.variant1;
 
 public class MusicStyles {
     public void playMusic() {
-        System.out.println("Playing music from the base MusicStyles class");
+        System.out.println("Playing music from the base MusicStyles class...");
     }
 }
 
